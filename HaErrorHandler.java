e02e8017e30c354ecca455ade2fbeb81ccba99e1
visual233 <264050673@qq.com>
2016-03-07 21:51:00 +0800

    Update HaErrorHandler.java

--- HaErrorHandler.java
+++ HaErrorHandler.java
@@ -3,7 +3,7 @@ class HaErrorHandler{
     String s = null;
     switch(code){
       case 1: s = "你们啊，naive！";break;
-	  case 2: s = "你们不要想喜欢弄个大bug";break;
+      case 2: s = "你们不要想喜欢弄个大bug";break;
     }
     System.out.print(s);
     System.exit(0);
